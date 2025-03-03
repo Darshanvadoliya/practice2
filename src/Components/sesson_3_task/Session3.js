1af01e3c827fd947795f33e4ce1de040d22ac729
Darshanvadoliya <vadoliyadarshan34@gmail.com>
2025-03-03 16:31:49 +0530

    .

--- src/Components/sesson_3_task/Session3.js
+++ src/Components/sesson_3_task/Session3.js
@@ -1,13 +1,26 @@
+// import { useState } from "react"
+
 function Session3({ name, age }) {
+    //  description ='werty ertyu rtyu tyu tyu'
+    // const [show, setShow] = useState(false)
+    // function heandleshowmore (){
+    //     setShow(!show)
+    // }
     return (
         <div className="main">
             <h1>Session 3</h1>
             <h2>task 1</h2>
             <h5>Name: {name}</h5>
             <h5>Age: {age}</h5>
+
+            {/* <button onClick={heandleshowmore}>
+                {show ? "Hide" : "show"}
+            </button>
+            {show && <p>{description}</p>} */}
         </div>
     )
 }
+
 Session3.defaultProps = {
     name: "Rahul",
     age: 20
